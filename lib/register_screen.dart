449e3e561461e1929880781c0314d71338935631
shehab <salshazlly3030@gmail.com>
2022-02-11 13:02:40 +0200

    validate with textFormfield to create account

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -1,9 +1,16 @@
+import 'package:chat_app/utils.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_files/custom_text.dart';
 
 class Registerscreen extends StatelessWidget {
   static final routeName = 'register screen';
+  String? firstName;
+  String? lastName;
+  String? userName;
+  String? email;
+  String? password;
+  var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -17,16 +24,125 @@ class Registerscreen extends StatelessWidget {
             fit: BoxFit.cover,
           )),
       child: Scaffold(
-        backgroundColor: Colors.transparent,
         appBar: AppBar(
           title: CustomText(
             text: 'Create account',
             fontWeight: FontWeight.bold,
             fontSize: 20,
           ),
-          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'First Name',
+                    ),
+                    onChanged: (text) {
+                      firstName = text;
+                    },
+                    validator: (text) {
+                      if (text == null || text.trim().isEmpty) {
+                        return 'Please enter first name';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Last Name',
+                    ),
+                    onChanged: (text) {
+                      lastName = text;
+                    },
+                    validator: (text) {
+                      if (text == null || text.trim().isEmpty) {
+                        return 'Please enter last name';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'User Name',
+                    ),
+                    onChanged: (text) {
+                      firstName = text;
+                    },
+                    validator: (text) {
+                      if (text == null || text.trim().isEmpty) {
+                        return 'Please enter user name';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                    ),
+                    onChanged: (text) {
+                      email = text;
+                    },
+                    validator: (text) {
+                      if (text == null || text.trim().isEmpty) {
+                        return 'Please enter email';
+                      }
+                      if (Utils.isValidEmail(email ?? '')) {
+                        return null;
+                      }
+                      return 'invalid email';
+                    },
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                    ),
+                    onChanged: (text) {
+                      email = text;
+                    },
+                    validator: (text) {
+                      if (text == null || text.trim().isEmpty) {
+                        return 'Please enter password';
+                      }
+                      if (text.length < 6) {
+                        return 'should be at least 6 character';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.07,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (formKey.currentState?.validate() == true) {
+                        createAccountWithFirebaseAuth();
+                      }
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Create account'),
+                          Icon(Icons.arrow_right),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
   }
+
+  void createAccountWithFirebaseAuth() {}
 }
